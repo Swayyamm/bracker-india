aa98f137cf756e3e382e59210edbc5d46e7e9a64
Swayyamm <swayammamtora2525@gmail.com>
2026-03-11 16:44:23 +0530

    fix react routing

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,6 @@ from backend.routes.admin_routes import admin_bp
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FRONTEND_DIST = os.path.join(BASE_DIR, "..", "frontend", "dist")
 
-
 app = Flask(
     __name__,
     static_folder=FRONTEND_DIST,
@@ -39,19 +38,17 @@ app.register_blueprint(chatbot_bp, url_prefix="/api/chatbot")
 
 
 # Serve React
-import os
-from flask import send_from_directory
-
-FRONTEND_DIST = os.path.join(os.getcwd(), "frontend", "dist")
-
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve_react(path):
 
-    if path != "" and os.path.exists(os.path.join(FRONTEND_DIST, path)):
+    file_path = os.path.join(FRONTEND_DIST, path)
+
+    if path != "" and os.path.exists(file_path):
         return send_from_directory(FRONTEND_DIST, path)
 
     return send_from_directory(FRONTEND_DIST, "index.html")
+
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
